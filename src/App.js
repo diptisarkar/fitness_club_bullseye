6e49dc10f66759a2b2862eb3cbe7fdba0223f3ca
Dipti Sarkar <73912658+diptisarkar@users.noreply.github.com>
2023-02-11 13:19:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,51 +31,9 @@ function App() {
   }
 
   return (
-    // <div className="form-style-6">
-    //   <form>
-    //     <input
-    //       type="text"
-    //       placeholder="Name"
-    //       onChange={handleChange}
-    //       name="firstName"
-    //     />
-    //     <br></br>
-    //     <input
-    //       type="email"
-    //       placeholder="Email"
-    //       onChange={handleChange}
-    //       name="email"
-    //     />
-    //     <br></br>
-    //     <input
-    //       type="number"
-    //       placeholder="Phone Number"
-    //       onChange={handleChange}
-    //       name="Phone"
-    //     />
-    //     <br></br>
-    //     <input
-    //       type="text"
-    //       placeholder="Address"
-    //       onChange={handleChange}
-    //       name="Address"
-    //     />
-    //     <br></br>
-    //     <button onClick={prompt}>Add Member</button>
-    //     <br></br>
-    //     <button onClick={members}>View Member List</button>
-    //     <br></br>
-    //   </form>
-    //   {/* <button onClick={prompt}>Add Member</button>
-    //   <button onClick={members}>View Member List</button> */}
 
-    //   <div className={show ? "visible" : "hidden"}>
-    //     <p>{`Name:${formData.firstName}`}</p>
-    //     <p>{`Email:${formData.email}`}</p>
-    //     <p>{`Phone Number:${formData.Phone}`}</p>
-    //     <p>{`Address:${formData.Address}`}</p>
-    //   </div>
-    // </div>
+
+  
 
     <div class="form-style-6">
       <h1>Fitness Club Registration</h1>
